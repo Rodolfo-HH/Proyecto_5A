ebc24fd63721b2b842a12aa6c2c7e7fac9457c52
Rodolfo Hernandez Hernandez <240836@utxicotepec.edu.mx>
2026-04-23 04:27:12 -0600

    Proyecto_5A
    add: correcion de error actualizar contreseña

--- Backend/routes/usuarios.js
+++ Backend/routes/usuarios.js
@@ -80,4 +80,43 @@ router.post('/login', (req, res) => {
     });
 });
 
+// =======================
+// CAMBIAR CONTRASEÑA
+// =======================
+router.post('/crear-password', async (req, res) => {
+    const { correo, password } = req.body;
+
+    console.log("📩 Datos recibidos:", correo, password);
+
+    if (!correo || !password) {
+        return res.status(400).json({ error: "Faltan datos" });
+    }
+
+    try {
+        const hash = await bcrypt.hash(password, 10);
+
+        const sql = "UPDATE usuarios SET password = ? WHERE correo = ?";
+
+        db.query(sql, [hash, correo], (err, result) => {
+
+            if (err) {
+                console.error("❌ ERROR SQL:", err);
+                return res.status(500).json({ error: "Error al actualizar contraseña" });
+            }
+
+            console.log("✅ Resultado:", result);
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ error: "Usuario no encontrado" });
+            }
+
+            res.json({ mensaje: "Contraseña actualizada correctamente 🔥" });
+        });
+
+    } catch (error) {
+        console.error("❌ ERROR SERVER:", error);
+        res.status(500).json({ error: "Error en servidor" });
+    }
+});
+
 module.exports = router;
